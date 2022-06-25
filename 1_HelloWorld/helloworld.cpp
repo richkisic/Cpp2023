93ee75e231618f362ee7446c11497d4214c7ef42
Rich Kisic <richard.kisic@gmail.com>
2022-06-25 18:15:27 -0400

    Initial commit

---
+++ 1_HelloWorld/helloworld.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+/*
+ A multi-line comment
+*/
+int main()
+{
+    // A single line comment
+    std::cout << "Hello World" /* an in-line comment */ << std::endl;
+    return 0;
+}
