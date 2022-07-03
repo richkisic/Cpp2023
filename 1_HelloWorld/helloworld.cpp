8922f6fe87d21231690bb5768735e4be78ba1fa9
Rich Kisic <richard.kisic@gmail.com>
2022-07-03 13:26:58 -0400

    Update helloworld.cpp

--- 1_HelloWorld/helloworld.cpp
+++ 1_HelloWorld/helloworld.cpp
@@ -1,11 +1,13 @@
 /*
- A multi-line comment
+ A multi-line 
+ comment
 */
 #include <stdio.h>
 
+// A single line comment
 int main()
 {
-    printf("2 Hello World");
+    printf("Hello World" /* an in-line comment */);
 
     return 0;
 }
